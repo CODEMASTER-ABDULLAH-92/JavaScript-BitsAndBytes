940760d8cc5e303ecaa87a2fa54f5a2d2ff62ce0
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-04-18 11:53:47 +0500

    April 18, Adding the Js control Structues

--- Js-Control-Structure/index.js
+++ Js-Control-Structure/index.js
@@ -1,5 +1,40 @@
+// 🟢 if
+// It checks a condition. If the condition is true, the code runs.
+
+// 🟡 else if
+// It checks another condition if the first if is false.
+
+// 🔵 if - else if
+// Used when you want to check multiple conditions, one after another.
+
+
+// 👉 if
+let name = "abdullah";
+if (name === "abdullah") {
+    console.log("Correct");
+}
+
+// 👉 else if
 let age = 18;
 
 if (age >= 18) {
-    console.log("Boy");
+  console.log("You are an adult");
+} else {
+  console.log("You are a minor");
 }
+
+
+// 👉 if else if
+let marks = 85;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 80) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C or below");
+}
+
+
+
+
